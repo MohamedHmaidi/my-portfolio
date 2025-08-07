242e984faa383d57773a067fc30104e57e5c6318
MohamedHmaidi <hmaidi185@gmail.com>
2025-08-07 20:11:20 +0100

    Initial commit with deploy setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
+  output: 'export',              
   basePath: isProd ? '/my-portfolio' : '',
   assetPrefix: isProd ? '/my-portfolio/' : '',
   trailingSlash: true,
