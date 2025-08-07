5210dca12be6b6ad58ac0f3aa1605d1b25d2e669
MohamedHmaidi <hmaidi185@gmail.com>
2025-08-07 20:45:52 +0100

    first

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  output: 'export',              
+  output: 'export',
   basePath: isProd ? '/my-portfolio' : '',
   assetPrefix: isProd ? '/my-portfolio/' : '',
   trailingSlash: true,
-}
+};
